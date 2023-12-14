376f642d6288aff016b5eeb6c55287f4849dcd1a
winter60 <ali60@qq.com>
2023-12-14 17:44:11 +0800

    修复未引用资源

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import moment from "moment";
 
 let onSyncEndEvent: EventListener;
 const STORAGE_NAME = "flomo-sync-config";
-const FLOMO_ASSETS_DIR = "/assets/flomo";
+const FLOMO_ASSETS_DIR = "assets/flomo";
 const USG = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.76";
 
 
